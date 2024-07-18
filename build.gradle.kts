032cf77c17722d1071227b375c323df307d0bc28
Øyvind Samuelsen <samu.developments@gmail.com>
2024-07-18 15:00:18 +0200

    set the toolchain version to corretto 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,13 @@ plugins {
     kotlin("jvm") version "2.0.0"
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+        vendor = JvmVendorSpec.AMAZON
+    }
+}
+
 group = "com.capra"
 version = "1.0-SNAPSHOT"
 
