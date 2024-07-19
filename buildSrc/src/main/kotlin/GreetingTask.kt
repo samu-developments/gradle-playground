270947cecceebf061cb478c1b8153f71db3d5ad1
Øyvind Samuelsen <samu.developments@gmail.com>
2024-07-19 11:55:50 +0200

    document, add convention value

--- buildSrc/src/main/kotlin/GreetingTask.kt
+++ buildSrc/src/main/kotlin/GreetingTask.kt
@@ -3,7 +3,6 @@ import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
-
 abstract class GreetingTask : DefaultTask() {
     @get:Input
     abstract val greetingName: Property<String>
--- buildSrc/src/main/kotlin/greeting-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/greeting-plugin.gradle.kts
@@ -1,16 +1,20 @@
 import gradle.kotlin.dsl.accessors._7fbf647f525c0b2fa279b9b076b8b9df.build
-import org.gradle.api.provider.Property
 
 interface GreetingTaskExtension {
     val nameProp: Property<String>
 }
 
+// Create the extension
 val greetingExtension = extensions.create<GreetingTaskExtension>("greeting")
 
+// Register the task and give it the input
 val greeting = tasks.register<GreetingTask>("greetingTask") {
     greetingName.convention(greetingExtension.nameProp)
 }
 
+greetingExtension.nameProp.convention(project.name)
+
+// Make task run on a lifecycle task
 tasks.build {
     dependsOn(greeting)
 }
